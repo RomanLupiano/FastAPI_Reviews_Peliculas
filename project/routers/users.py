db5bb3825711ceebbc92e3a274e88087eabf6d7e
RomanLupiano <romanlupiano@gmail.com>
2024-06-25 14:23:08 -0300

    Simple login

--- project/routers/users.py
+++ project/routers/users.py
@@ -1,8 +1,11 @@
-from fastapi import HTTPException, APIRouter
+from fastapi import HTTPException, APIRouter, Response, Cookie
+from fastapi.security import HTTPBasicCredentials
 
 from ..database import User
 
-from ..schemas import UserResponseModel, UserRequestModel
+from ..schemas import UserResponseModel, UserRequestModel, ReviewResponseModel
+
+from typing import List
 
 router = APIRouter(prefix='/users')
 
@@ -19,3 +22,26 @@ async def create_user(user: UserRequestModel):
     )
     
     return user
+
+@router.post('/login', response_model=UserResponseModel)
+async def login(credentials: HTTPBasicCredentials, response: Response):
+    user = User.select().where(User.username == credentials.username).first()
+    
+    if user is None:
+        raise HTTPException(404, 'User not found')
+    
+    if user.password != User.create_password(credentials.password):
+        raise HTTPException(404, 'Password error')
+    
+    response.set_cookie(key='user_id', value=user.id) #TOKEN
+    return user
+
+@router.get('/reviews', response_model=List[ReviewResponseModel])
+async def get_reviews(user_id: int = Cookie(None)):
+    
+    user = User.select().where(User.id == user_id).first()
+    
+    if user is None:
+        raise HTTPException(303, 'User not found')
+    
+    return [user_review for user_review in user.reviews]
